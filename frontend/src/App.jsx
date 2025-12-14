2da754cb14f46e0edaea69a827d5fcbf11342293
Keshav <keshav.poha@gmail.com>
2025-12-15 00:02:38 +0530

    Added Dummy Landing Page, LoginUI, Dashboard page, Profile Setup UI, all with dummy brandkit (#19)
    * LoginUI: Implement Google OAuth login UI component
    
    * Added DashboardPage and ProfileSetupUI components
    
    * fix
    
    * small Update to package-lock.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage'
+import { DashboardPage } from './pages/DashboardPage'
 
-const App = () => {
+function App() {
   return (
-    <div>DSABUDDY</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
   )
 }
 
