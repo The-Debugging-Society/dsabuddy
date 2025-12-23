a4c35579c3eac2b7a9d5f868b24969c22a1a7440
Bhoomi <imbhoomigupta@gmail.com>
2025-12-23 14:14:18 +0530

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-
-=======
-import tailwindcss from '@tailwindcss/vite'
->>>>>>> 403f149db495ae53d2395af5c88816739dad0610
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [
     react(),
   ],
-=======
-  plugins: [react(), tailwindcss()],
->>>>>>> 403f149db495ae53d2395af5c88816739dad0610
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
